43d61e4789afbfcb85b6a8d1341ab49bca743f83
Shogo Kawaguchi <platycod0n.ramosa@gmail.com>
2015-01-09 14:22:17 +0900

    ノート本文をマークダウンで記述出来る

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -1,2 +1,17 @@
 module NotesHelper
+  @@renderer = Redcarpet::Render::HTML.new filter_html: true, hard_wrap: true
+
+  @@markdown = Redcarpet::Markdown.new @@renderer,
+    autolink:            true,
+    space_after_headers: true,
+    no_intra_emphasis:   true,
+    fenced_code_blocks:  true,
+    tables:              true,
+    xhtml:               true,
+    lax_html_blocks:     true,
+    strikethrough:       true
+
+  def markdown(text)
+    @@markdown.render(text).html_safe
+  end
 end
